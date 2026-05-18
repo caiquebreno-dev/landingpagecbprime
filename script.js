1bf0a2f70a8999f02ade1a8f3d6c9bf98bb3103b
Caíque Breno <caiquebreno965@gmail.com>
2026-05-18 16:17:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// Animação de Partículas no Fundo
+const canvas = document.getElementById('particles-canvas');
+const ctx = canvas.getContext('2d');
+
+let particlesArray = [];
+let w, h;
+
+function initCanvas() {
+  w = canvas.width = window.innerWidth;
+  h = canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', initCanvas);
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * w;
+    this.y = Math.random() * h;
+    this.size = Math.random() * 2;
+    this.speedX = Math.random() * 1 - 0.5;
+    this.speedY = Math.random() * 1 - 0.5;
+    this.opacity = Math.random() * 0.4 + 0.1;
+  }
+  
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    
+    // Reposiciona a partícula se ela sair da tela
+    if (this.x < 0) this.x = w;
+    if (this.x > w) this.x = 0;
+    if (this.y < 0) this.y = h;
+    if (this.y > h) this.y = 0;
+  }
+  
+  draw() {
+    ctx.fillStyle = `rgba(201, 168, 76, ${this.opacity})`;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+}
+
+function initParticles() {
+  particlesArray = [];
+  // Reduz a quantidade de partículas em telas menores para melhor performance
+  const numberOfParticles = Math.min(window.innerWidth / 15, 80);
+  for (let i = 0; i < numberOfParticles; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animateParticles() {
+  ctx.clearRect(0, 0, w, h);
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+  }
+  requestAnimationFrame(animateParticles);
+}
+
+// Inicia
+initCanvas();
+initParticles();
+animateParticles();
